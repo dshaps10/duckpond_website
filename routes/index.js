62d7bfbec25bbd6fcced274ab1f6830bb1a67fd6
dshaps10 <dan.shapiro1210@gmail.com>
2018-01-13 09:38:20 -0800

    Add about page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Get home page
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+// GET about page
+router.get('/about', function(req, res, next) {
+  res.render('about');
 });
 
 module.exports = router;
