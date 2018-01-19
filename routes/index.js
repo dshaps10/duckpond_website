6d5f3da678ab99f100dfc987b5fe0a4b58f51f21
dshaps10 <dan.shapiro1210@gmail.com>
2018-01-18 18:18:43 -0800

    Add faq page and delete excess styling

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/about', function(req, res, next) {
   res.render('about');
 });
 
+// GET FAQ page
+router.get('/faq', function(req, res, next) {
+  res.render('faq');
+});
+
 module.exports = router;
