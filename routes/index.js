d2971b7896df1e59719aa8ae427eb8a2824b07d1
Daniel Shapiro <dshapiro@us-dshapiro2.local>
2018-05-26 09:33:58 -0700

    Delete old homepage templates

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var password = require('../password');
 
 // Get home page
 router.get('/', function(req, res, next) {
-  res.render('index3');
+  res.render('index');
   console.log(emails.emails.includes('dan.shapiro1210@gmail.com'));
 });
 
